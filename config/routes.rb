89d90dfc986ebf52f7e0c7c4c1df9d5666f087c2
Alan Fidelino <afidedev26@gmail.com>
2016-06-17 22:34:27 +1000

    3:11 Add faq page and carousel with styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'contact' => 'static_pages#contact'
 
-  get 'static_pages/index'
+  get 'faq'     => 'static_pages#faq'
   
   root 'static_pages#index'
 end
