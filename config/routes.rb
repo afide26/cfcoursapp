baa0a21de4e52d8369a7f61fd34d71e96ade1f29
Alan Fidelino <afidedev26@gmail.com>
2016-06-17 21:28:27 +1000

    3:11 Added links and styling to navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get 'about'   => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
 
   get 'static_pages/index'
   
