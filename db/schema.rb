1e61ec49d5eda3cb9549e521146ec225fccd2fb6
Alan Fidelino <afidedev26@gmail.com>
2016-06-19 20:12:54 +1000

    4.2 Add price to the products table with styling on pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618140135) do
+ActiveRecord::Schema.define(version: 20160619095555) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160618140135) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.decimal  "price"
   end
 
 end
