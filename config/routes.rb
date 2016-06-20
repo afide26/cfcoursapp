1e9431da3bc1a998f28c1a4a2865a791b233401c
Alan Fidelino <afidedev26@gmail.com>
2016-06-20 21:41:39 +1000

    4.3 Added Orders controller and resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'about'   => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
