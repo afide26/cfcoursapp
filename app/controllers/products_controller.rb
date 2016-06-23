771c0901f117114007031cdeb65095ef931f060e
Alan Fidelino <afidedev26@gmail.com>
2016-06-23 20:49:07 +1000

    4.5 Revision to have dynamic contents for root/landing_page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,6 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def landing_page
-     @products = Product.all
-  end
 
   # GET /products/1
   # GET /products/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   get 'about'   => 'static_pages#about'
 
-  get 'landing' => 'products#landing_page'
+  get 'index' => 'static_pages#index'
 
   get 'contact' => 'static_pages#contact'
 
