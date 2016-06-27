b385ac4138a05387bf7fd13b459716926bdb918e
Alan Fidelino <afidedev26@gmail.com>
2016-06-27 22:03:24 +1000

    5.1 Routes and flash message styling and logic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to :back, notice: 'Product was successfully created.' }
+        format.html { redirect_to @product, :success => 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to session.delete(:return_to), notice: 'Product was successfully updated.' }
+        format.html { redirect_to session.delete(:return_to), :info=>'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -58,8 +58,8 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, danger: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to products_path, :danger=>'Product was successfully destroyed.' }
+      # format.json { head :no_content }
     end
   end
 
