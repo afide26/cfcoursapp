3b49435178b3f3d7638023b1f9f3fcc4ddc13a84
Alan Fidelino <afidedev26@gmail.com>
2016-06-29 19:49:48 +1000

    5.3 Sending emails fix code in user_mailer.rb and application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "afidedev26@gmail.com"
+  default from: "from@example.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "afidedev26@gmail.com"
+  default from: "from@example.com"
 
   def contact_form(email, name, message)
     @message = message
