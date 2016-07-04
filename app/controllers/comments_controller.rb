11ce0ea424da03d6a8342f44421957105a175b9c
Alan Fidelino <afidedev26@gmail.com>
2016-07-04 21:09:25 +1000

    5.10 AAdding admin role to application

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+
+    @comment.destroy
+    redirect_to product
   end
 
   private
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
 end
