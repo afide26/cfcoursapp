186eca9a3a0204df69dc11bfe432f9e03ef47502
Alan Fidelino <afidedev26@gmail.com>
2016-07-12 20:52:00 +1000

    Add title helper to customise title for all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_page_title(page_title=" ")
+    base_title = "Bike Berlin"
+    if page_title.empty?
+      "#{base_title}"
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
