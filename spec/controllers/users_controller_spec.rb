c9994796dd6e9c1ff197ed68e88ec1f256b86db6
Alan Fidelino <afidedev26@gmail.com>
2016-07-13 20:40:11 +1000

    Users controller spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  let(:user) {User.create!(email:"alan@example.com", password: "fidelino1126")}
+
+  describe "GET #show" do
+
+    context "User is logged in" do
+      before do
+        sign_in user
+      end
+
+      it "loads correct user details" do
+        get :show, id: user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq user
+      end
+    end
+
+    context "No user is logged in" do
+      it "redirects to login" do
+        get :show, id: user.id
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  let(:user_two) {User.create!(email:"carl@example.com", password: "fidelino0715")}
+
+  describe "GET #edit" do
+    context "User is logged in and tries to edit profile" do
+      before do
+        sign_in user_two
+      end
+
+      it "should redirect to the users edit page" do
+        get :edit, id: user_two.id
+        expect(response).to have_http_status(200)
+      end
+
+      it "should block other users from editing pages" do
+        get :edit, id: user.id
+        expect(response).not_to be_success
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
+  describe "GET #show" do
+    context "User viewing other user profiles" do
+      before do
+        sign_in :user_two
+      end
+
+      it "should block user and redirected to root path " do
+        get :show, id: user.id
+        expect(response).to redirect_to("/")
+      end
+    end
+  end
+end
