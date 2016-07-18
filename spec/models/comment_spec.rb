8610632efba8261211801ac5bf6642b454808575
Alan Fidelino <afidedev26@gmail.com>
2016-07-18 20:02:18 +1000

    6.5 Automating tests using guard and spork

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,7 +12,7 @@ describe Comment do
     expect(Comment.new(body:"Awesome bicycle", user:@user, rating:0)).to_not be_valid
    end
 
-   it "should have a body with a minimum of 10 characters" do
+   it "should have a body with a min of 10 characters" do
     @comment = @product.comments.create(body:"Awesome bicycle", user:@user, rating:0)
     expect(@comment.body.length >= 10).to be_truthy
    end
