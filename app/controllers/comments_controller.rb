1d4d6ba7fa9a4243b577a3e5751dc981e29b0c09
Alan Fidelino <afidedev26@gmail.com>
2016-07-25 22:24:57 +1000

    6.9 Add byebug gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   before_filter :authorize_admin, only: [:destroy]
-  
+
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
     respond_to do |format|
-      if @comment.save 
+      if @comment.save
         format.html{redirect_to @product, success: 'Comment was created successfully'}
         format.json{render :show, status: :created, location: @product}
       else
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:user_id,:body, :rating)
     end
-    
+
     def authorize_admin
       return unless !current_user.admin?
       redirect_to root_path, :alert => "Only admins can delete comments"
