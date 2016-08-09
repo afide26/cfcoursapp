229b375f20e736c0af3def1e720472268987f841
Alan Fidelino <afidedev26@gmail.com>
2016-08-09 19:01:16 +1000

    Fix star rating for comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,15 @@
-$(document).on('turbolinks: load', function(){
-  $('.rating').raty({ path:'/assets', scoreName: 'comment[rating]'})
+var refreshRating = function() {
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
   $('.rated').raty({ path: '/assets',
-                     readOnly: true,
-                     score: function(){
-                      return $(this).attr('data-score');
-                     }
-                    });
-  $(".img-zoom").elevateZoom();
-})
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
+
+$(document).on('turbolinks:load', function() {
+  refreshRating();
+
+  $('.img-zoom').elevateZoom();
+});
