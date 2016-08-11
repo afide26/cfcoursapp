15446530a9c3e5764738e718b8b3c1e85ea58f73
Alan Fidelino <afidedev26@gmail.com>
2016-08-11 22:34:12 +1000

    Broadcast comments stream from action cable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     respond_to do |format|
       if @comment.save
+        ActionCable.server.broadcast 'product_channel', comment: @comment
         format.html{redirect_to @product, success: 'Comment was created successfully'}
         format.json{render :show, status: :created, location: @product}
         format.js
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://afbikeberlin.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://afbikeberlin.herokuapp.com', 'http://afbikeberlin.herokuapp.com']
 end
