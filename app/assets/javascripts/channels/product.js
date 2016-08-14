b0495a7c4e8c2a2898b59e1a29b5ef245a9af1ed
Alan Fidelino <afidedev26@gmail.com>
2016-08-14 20:51:31 +1000

    Add code from careerfoundry sample app

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,4 +1,4 @@
-App.product = App.cable.subscriptions.create("ProductChannel", {
+App.product = App.cable.subscriptions.create({channel: "ProductChannel"}, {
   connected: function() {
     // Called when the subscription is ready for use on the server
   },
@@ -10,5 +10,17 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
+    $('.product-reviews').prepend(data.comment);
+    refreshRating();
+  },
+
+  listen_to_comments: function() {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
   }
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
