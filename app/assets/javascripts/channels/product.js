ebac840b4b467d41b1c29e459ec83f92d939bd5b
Alan Fidelino <afidedev26@gmail.com>
2016-08-16 21:29:06 +1000

    6.13 Real-Time Updates with action cable

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,4 +1,4 @@
-App.product = App.cable.subscriptions.create("ProductChannel", {
+App.cable.subscriptions.create({channel: "ProductChannel"}, {
   connected: function() {
     // Called when the subscription is ready for use on the server
   },
@@ -9,9 +9,11 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    $(".alert.alert-info").show();
-    $('.product-reviews').prepend(data.comment);
-    refreshRating();
-  }
+    $("#cable-flash").show();
+    $(".product-reviews").prepend(data);
+  },
 });
+
+
+
 
