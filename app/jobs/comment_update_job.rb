bfc2c39b54c650c4402a8b794bdcf63634921575
Alan Fidelino <afidedev26@gmail.com>
2016-08-23 22:11:17 +1000

    Revert to working action cable code

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -1,13 +1,13 @@
 class CommentUpdateJob < ApplicationJob
   queue_as :default
 
-  def perform(comment, current_user)
-    ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
+  def perform(comment) #current_user has been removed
+    ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, comment.user), avg_rating: comment.product.avg_rating
   end
 
-  private
+private
 
-    def render_comment(comment, current_user)
-      CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })
-    end
+  def render_comment(comment, current_user)
+    CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })
+  end
 end
