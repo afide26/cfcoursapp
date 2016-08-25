b08fd814abf25b7359ce24b55ae7d8f3d82cc101
Alan Fidelino <afidedev26@gmail.com>
2016-08-25 19:13:26 +1000

    Change cable channel code

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,4 +1,4 @@
-App.product = App.cable.subscriptions.create({channel: "ProductChannel"}, {
+App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
   },
