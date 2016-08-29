31941eb46188e921b6af0d0174ee41e5737485c8
Zubair Shams <m.zubair.shams@gmail.com>
2016-08-29 16:19:41 +0500

    action cable without job and move images into assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://afbikeberlin.herokuapp.com/cable'
   #config.action_cable.allowed_request_origins = [ 'https://afbikeberlin.herokuapp.com', 'http://afbikeberlin.herokuapp.com' ]
   #config.action_cable.allowed_request_origins = [ 'https://afbikeberlin.herokuapp.com', /http:\/\/example.*/ ]
 
